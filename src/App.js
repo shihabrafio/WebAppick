315cd17a64170ec1d6f4d33a2dfc41de8ff141a4
shihabrafio <aimshihabuddinkhan@gmail.com>
2024-08-18 18:12:56 +0600

    feat: Add second page component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
 import { Firstpage } from './components/firstpage';
+import { Secondpage } from './components/secondpage';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Firstpage/>
+      <Secondpage/>
     </div>
   );
 }
