6c0efdd66fc63f8e727f89c5ea2446c93071348d
shihabrafio <aimshihabuddinkhan@gmail.com>
2024-08-18 18:13:01 +0600

    chore: Update first page component styles and assets

--- src/components/firstpage.js
+++ src/components/firstpage.js
@@ -11,7 +11,7 @@ export const Firstpage = () => {
         container: {
             display: 'flex',
             justifyContent: 'space-evenly',
-            gap: '19%',
+            gap: '12%',
             backgroundColor: '#245D51',
             color: '#fff',
         },
@@ -23,6 +23,9 @@ export const Firstpage = () => {
             fontWeight: 600,
             textAlign: 'left',
             marginLeft: '57px',
+            backgroundImage: `url(${vector1})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: '15rem 5.5rem',
         },
         button:{
             display: 'flex',
@@ -32,7 +35,11 @@ export const Firstpage = () => {
             borderRadius: '5px',
             cursor: 'pointer',
             fontSize: '20px',
+            paddingRight: '30px',
+            paddingLeft: '20px',
             fontWeight: 700,
+            width: '210px',
+            height: '40px',
         },
         p:{
             textAlign: 'left',
@@ -40,28 +47,59 @@ export const Firstpage = () => {
             fontWeight: 400,
             marginLeft: '57px',
             paddingRight: '6.5rem',
-        }
+        },
+        buttonImg:{
+            marginLeft: '10px',
+            marginTop: '14px',
+            width: '12px',
+            height: '12px',
 
+        },
+        logos:{
+            display: 'flex',
+            gap: '20px',
+            marginTop: '6px',
+        },
+        logo:{
+            width: '30px',
+            height: '30px',
+        },
+        part_3:{
+            marginLeft: '58px',
+            display: 'flex',
+            gap: '20px',
+            marginBottom: '4rem',
+        },
+        button_head:{
+            marginTop: '5px',
+            marginBottom: '0px',
+            fontSize: '20px',
+        },
+        rightside:{
+            marginRight: '8rem',
+            marginTop: '3rem',
+        },
     }
 
   return (
     <div style={styles.container}>
         <div style={styles.left}>
-            <h1 style={styles.h1}>There is always something new for us to learn</h1>
-            <img src={vector1} alt="" />
+            <h1 style={styles.h1}>There is always something  new for us to learn</h1>
             <p style={styles.p}>we have created more than 100+ materials on various things that will help your career, with mentors who are experienced in their fields.</p>
-            <div>
+            <div style={styles.part_3}>
                 <button style={styles.button}>
-                    <h6>Start Journey</h6>
-                    <img src={ic} alt="button" />
+                    <h6 style={styles.button_head}>Start Journey</h6>
+                    <img style={styles.buttonImg} src={ic} alt="button" />
                 </button>
-                <img src={insta} alt="logos" />
-                <img src={x} alt="logos" />
-                <img src={disco} alt="logos" />
+                <div style={styles.logos}>
+                    <img src={insta} style={styles.logo} alt="logos" />
+                    <img src={x} style={styles.logo} alt="logos" />
+                    <img src={disco} style={styles.logo} alt="logos" />
+                </div>
             </div>
         </div>
         <div>
-            <img src={hg} alt="lady" />
+            <img style={styles.rightside} src={hg} alt="lady" />
         </div>
     </div>
   )
